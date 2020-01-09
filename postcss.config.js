ca2f79dfb213730dd290d486c6968083ea4eeb26
ulfschneider <u@ulf.codes>
2020-01-09 11:48:38 +0100

    Minify with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('postcss-custom-media'),
-      require('postcss-custom-properties', {
-        preserve: true
-      }),
-      require('postcss-calc'),
-      require('cssstats'),
-      require('postcss-discard-comments'),
-      require('autoprefixer'),
-      require('postcss-reporter')
-    ]
-  }
+  plugins: [
+    require('postcss-import'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties', {
+      preserve: true
+    }),
+    require('postcss-calc'),
+    require('cssstats'),
+    require('postcss-discard-comments'),
+    require('autoprefixer'),
+    require('postcss-reporter'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ]
+}
