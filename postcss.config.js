09741c772a0db1bdcc1640b7964c110fb6de2abf
ulfschneider <u@ulf.codes>
2020-10-25 06:05:43 +0100

    Improve postcss pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,12 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-custom-media'),
-        require('postcss-custom-properties'),
         require('postcss-calc'),
-        require('postcss-discard-comments'),
-        require('autoprefixer'),
-        require('postcss-reporter'),
-        require('cssstats'),
-        require('cssnano'),
+        require('cssnano')({
+            preset: ['default', {
+                minifySelectors: false
+            }]
+        })
+
     ]
 }
