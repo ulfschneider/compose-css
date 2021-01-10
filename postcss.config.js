a726577b8c29e307f0e092eb06343871d49fc5d7
ulfschneider <u@ulf.codes>
2021-01-10 08:44:27 +0100

    Rename and preserve breakpoints

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('postcss-custom-media'),
+        require('postcss-custom-media')({
+            preserve: true
+        }),
         require('postcss-calc'),
         require('cssnano')({
             preset: ['default', {
