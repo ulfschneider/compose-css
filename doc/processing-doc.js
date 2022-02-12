b1a4df9140d77aaa61f1a4b423976b6e6bac3cc7
ulfschneider <mail@ulf.codes>
2022-02-12 08:47:46 +0100

    Change vertical rhythm

--- doc/processing-doc.js
+++ doc/processing-doc.js
@@ -32,7 +32,7 @@ function getMarkdownLib() {
         .use(markdownItTableOfContents, { containerClass: 'table-of-contents fluid-columns-5 bleed-right' })
         .use(markdownItDefList)
         .use(markdownItFitMedia, {
-            imgDir: './content/'
+            imgDir: './'
         })
         .use(markdownItScrollTable)
         .use(markdownItAttrs)
